e84dc000846af8396458ab0329dd93ac5d8ee775
0ushen <samir.benlafya@gmail.com>
2017-06-14 17:08:41 +0200

    sans /liste

---
+++ ScriptProjetOS.ps1
@@ -0,0 +1,27 @@
+﻿function StartOrStopService
+{
+    param(
+        [string]$service,
+        [string]$action
+    )
+    switch ($action){
+        'start'{
+            Start-Service $service
+        }
+        'stop'{
+            Stop-Service $service
+        }
+        default{
+            Write-Host 'error'
+        }
+    }
+}
+
+function ADNewUser
+{
+    [string]$name = Read-Host "Nom de l'utilisateur"
+    [string]$domain = Read-Host "Nom du domaine"
+    $password = Read-Host -AsSecureString "Mot de passe"
+
+    New-ADUser -name $name -server $domain -AccountPassword $password -Enabled $true
+}
